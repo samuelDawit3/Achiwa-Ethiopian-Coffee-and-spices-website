7de622950cbaba728d1a957cb03adf665bd2ce0c
Samuel Asfaw <samueldawitasfaw@gmail.com>
2026-06-21 18:44:20 +0200

    Change app.run to use host and port parameters

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def contact():
         return redirect(url_for('contact'))
     return render_template('contact.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
